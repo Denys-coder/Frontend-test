676d84299c560faf672439b14c7bafa077dd02d5
Denys Lapshyn <denislapshin1@gmail.com>
2024-05-24 20:13:17 +0300

    added showing of devicePixelRatio

--- script.js
+++ script.js
@@ -12,8 +12,16 @@
 alert("document.documentElement.clientWidth: " + document.documentElement.clientWidth); // 1536
 alert("document.documentElement.clientHeight: " + document.documentElement.clientHeight); // 1536
 alert("window.innerWidth: " + window.innerWidth); // 730
-alert("window.innerHeight: " + window.innerHeight); // 730
+alert("window.innerHeight: " + window.innerHeight);
 
-// personal laptop - 1536x730
-// working laptop - 1280x585
-// mobile - 360x710
+alert("window.devicePixelRatio: " + window.devicePixelRatio);
+
+// personal laptop - 1920x945
+// working laptop - 1920x945
+// mobile with viewport - 360x710, 360x710, square = 5/6
+// mobile without viewport - 360x1932, 980x1933, square = slightly smaller 1/3
+
+// increased scale = decreased resolution
+// decreased scale = increased resolution
+
+// firstly page gets rendered according to the viewport and then each element gets scaled up or down
